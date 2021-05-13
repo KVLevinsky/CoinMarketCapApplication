f10789a489aca4525dc3249fed3747170e5214f0
Konstantin Levinsky <kvlevinsky@outlook.com>
2021-05-13 14:16:12 +0200

    Altering for support the .NET Core 3.1

---
+++ CoinMarketCapApplication-v31/Models/CoinDataModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoinMarketCapApplication_v31.Models {
+    public class CoinDataModel {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Symbol { get; set; }
+
+        public string LogoURL { get; set; } = string.Empty;
+
+        [DisplayFormat(DataFormatString = "{0:N5}")]
+        public decimal Price { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N5}")]
+        public decimal Capitalization { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N4}%")]
+        public float Change1H { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N4}%")]
+        public float Change24h { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:D}", HtmlEncode = false)]
+        public DateTime LastUpdated { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:g}", HtmlEncode = false)]
+        public DateTime LastUpdatedLocalTime {
+            get {
+                return LastUpdated.ToLocalTime();
+            }
+        }
+    }
+}
---
+++ CoinMarketCapApplication-v31/Services/ICoinMarketCapCache.cs
@@ -0,0 +1,12 @@
+﻿using CoinMarketCapApplication_v31.Models;
+using System.Collections.Generic;
+
+namespace CoinMarketCapApplication_v31.Services {
+    public interface ICoinMarketCapCache {
+        void AddData(string data);
+        void AddAuxData(string data);
+        List<string> GetEmptyLogoIds();
+        dynamic LastValuableData { get; }
+        List<CoinDataModel> Data { get; }
+    }
+}
